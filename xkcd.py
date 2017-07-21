341c867dae485a559868013f9c69b023753ad1db
mahnoormian <mahnoormian@berkeley.edu>
2017-07-20 18:44:47 -0700

    Update xkcd.py

--- xkcd.py
+++ xkcd.py
@@ -10,3 +10,25 @@ soup = bs(page)
 
 #initiate count to track images
 count = 0 
+
+for i in range(0,5):  
+   #find the images
+    for image in soup.find_all("img"):
+ 
+  #find relevant image
+      try:
+        image_url = image["img src"]
+        print(image_url)
+        #create filename for image
+        filename = './' + str(count) + "xkcd.jpg"
+        #download image
+        urllib.request.urlretrieve(image_url, filename)
+        count += 1
+    #only go to except block if try block failed
+    except Exception as e:
+        pass
+                                     
+  #open previous link
+  prevLink = soup.select('a[rel="prev"]')
+  url = 'http://xkcd.com' + prevLink.get('href')
+                                     
